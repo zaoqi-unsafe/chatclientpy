0c5e9faab24938b5491d58bf9e348b3162138c44
zaoqi-unsafe <zaoqi-unsafe@users.noreply.github.com>
2019-02-04 08:58:50 +0800

    Update client.py

--- client.py
+++ client.py
@@ -5,6 +5,10 @@ class Group:
         self.nick = nick
         self.id = id
         self._message_callbacks : List[Callable[[User, Message], None]] = []
+        @client.on_group_message
+        def _handle_client_grpmsg(group : Group, sender : User, message : Message) -> None:
+            if group == self:
+                self._do_on_message(sender, message)
     def on_message(self):
         def do_register(func : Callable[[User, Message], None]) -> None:
             self._message_callbacks.append(func)
@@ -14,13 +18,17 @@ class Group:
             callback(sender, message)
     @property
     def alive(self) -> bool:
-        return False
+        return self.client.alive
 class User:
     def __init__(self, client : Client, nick : str, id : str):
         self.client = client
         self.nick = nick
         self.id = id
         self._message_callbacks : List[Callable[[Message], None]] = []
+        @client.on_private_message
+        def _handle_client_privmsg(sender : User, message : Message) -> None:
+            if sender ==  self:
+                self._do_on_message(message)
     def on_message(self):
         def do_register(func : Callable[[Message], None]) -> None:
             self._message_callbacks.append(func)
@@ -30,7 +38,7 @@ class User:
             callback(sender, message)
     @property
     def alive(self) -> bool:
-        return False
+        return self.client.alive
 class Message:
     def __init__(self, message : str):
         self.message : str = message
@@ -59,6 +67,12 @@ class Client:
     @property
     def alive(self) -> bool:
         return False
+    @property
+    def groups(self) -> List[Group]:
+        return []
+    @property
+    def friends(self) -> List[User]:
+        return []
 
 import wxpy
 class WxpyGroup(Group):
@@ -73,7 +87,7 @@ class WxpyUser(User):
             pass
     @property
     def alive(self) -> bool:
-        return self._wxbot.alive()
+        return self._wxbot.alive
    
 class WxpyClient(Client):
     def __init__(self):
